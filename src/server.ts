c5ae768644a25971675ebc12949285205c91f798
KarlGNassar <kgnassar01@gmail.com>
2021-12-28 01:18:44 +0200

    todo validation

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
 import { v4 as uuidv4 } from "uuid";
 
 import db from "./config/database.config";
 import { TodoInstance } from "./model";
+import TodoValidator from "./validators";
 
 const app = express();
 const port = 9000;
@@ -17,16 +19,27 @@ app.get("/healthcheck", (req: Request, res: Response) => {
   return res.send("server is running.. 🏃‍♂️");
 });
 
-app.post("/create", async (req: Request, res: Response) => {
-  const id = uuidv4();
-  try {
-    const record = await TodoInstance.create({ ...req.body, id });
-    return res.json({ record, msg: "Successfully created todo" });
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ msg: "Failed to create", status: 500, route: "/create" });
+app.post(
+  "/create",
+  TodoValidator.checkCreateTodo(),
+  (req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+
+    if (!error.isEmpty()) return res.json(error);
+
+    next();
+  },
+  async (req: Request, res: Response) => {
+    const id = uuidv4();
+    try {
+      const record = await TodoInstance.create({ ...req.body, id });
+      return res.json({ record, msg: "Successfully created todo" });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ msg: "Failed to create", status: 500, route: "/create" });
+    }
   }
-});
+);
 
 app.listen(port, () => console.log(`listening on localhost:${port}`));
---
+++ src/validators/index.ts
@@ -0,0 +1,21 @@
+import { body } from "express-validator";
+
+class TodoValidator {
+  checkCreateTodo() {
+    return [
+      body("id")
+        .optional()
+        .isUUID(4)
+        .withMessage("The value should be UUID v4"),
+      body("title").notEmpty().withMessage("The title should not be empty"),
+      body("completed")
+        .optional()
+        .isBoolean()
+        .withMessage("The value should be a boolean")
+        .isIn([0, false])
+        .withMessage("The value should be 0 or false"),
+    ];
+  }
+}
+
+export default new TodoValidator();
