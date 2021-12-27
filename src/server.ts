b20d565216a94c21f412ef2a7a96664440ccd51f
KarlGNassar <kgnassar01@gmail.com>
2021-12-28 00:52:24 +0200

    database setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
 
+import db from "./config/database.config";
+
 const app = express();
 const port = 9000;
 
+db.sync().then(() => {
+  console.log("DB Connected");
+});
+
 app.get("/", (req: Request, res: Response) => {
-  return res.send("hello world");
+  return res.send("server is running.. 🏃‍♂️");
 });
 
 app.listen(port, () => console.log(`listening on localhost:${port}`));
