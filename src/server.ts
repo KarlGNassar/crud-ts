0af89c4d8d2fdc6d0ac9426011ab9a23201b7e60
KarlGNassar <kgnassar01@gmail.com>
2021-12-28 00:48:08 +0200

    express setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 9000;
+
+app.get("/", (req: Request, res: Response) => {
+  return res.send("hello world");
+});
+
+app.listen(port, () => console.log(`listening on localhost:${port}`));
