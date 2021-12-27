7a61788a6f61a6218278ad42395fa86b7d3f742e
KarlGNassar <kgnassar01@gmail.com>
2021-12-28 01:08:31 +0200

    create todo

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,32 @@
 import express, { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
 
 import db from "./config/database.config";
+import { TodoInstance } from "./model";
 
 const app = express();
 const port = 9000;
+
+app.use(express.json());
 
 db.sync().then(() => {
   console.log("DB Connected");
 });
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/healthcheck", (req: Request, res: Response) => {
   return res.send("server is running.. 🏃‍♂️");
+});
+
+app.post("/create", async (req: Request, res: Response) => {
+  const id = uuidv4();
+  try {
+    const record = await TodoInstance.create({ ...req.body, id });
+    return res.json({ record, msg: "Successfully created todo" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: "Failed to create", status: 500, route: "/create" });
+  }
 });
 
 app.listen(port, () => console.log(`listening on localhost:${port}`));
